ba3e6ce1b69c62bd282f1b329645394dbeef9e1a
Cory Baker <coryb08@gmail.com>
2025-10-09 10:18:25 -0500

    first commit

---
+++ src/app/board/board.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { BoardModel } from '../models/board.model';
+import { CommonModule } from '@angular/common';
+@Component({
+  selector: 'app-board',
+  imports: [CommonModule],
+  templateUrl: './board.html',
+  styleUrl: './board.scss'
+})
+export class Board {
+  basePositions = BoardModel.basePositions;
+}
